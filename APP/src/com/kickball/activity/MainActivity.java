998ae676529364d47a39b6021f4d7e5f49335a6b
sandeco <sandecom@gmail.com>
2014-11-19 06:47:26 -0200

    Desenvolvimento de Jogos para Android

--- APP/src/com/kickball/activity/MainActivity.java
+++ APP/src/com/kickball/activity/MainActivity.java
@@ -1,26 +1,33 @@
 package com.kickball.activity;
 
-import com.kickball.R;
-import com.kickball.game.KickBallController;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.kickball.R;
+
 public class MainActivity extends Activity {
 
-	KickBallController kickball;
-	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		kickball = new KickBallController(this);				
-		setContentView(kickball);
+		
 	}
 
 
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		//kickball.resume();
+		
+	}
+
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,11 +35,6 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		kickball.resume();
-	}
 	
 	
 	
